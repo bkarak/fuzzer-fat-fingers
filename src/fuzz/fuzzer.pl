19860221892e0097b1ee8a43c1b6ee3a8eddbd2d
Diomidis Spinellis <dds@aueb.gr>
2012-08-08 19:19:25 +0300

    Add tokenization and testing

--- src/fuzz/fuzzer.pl
+++ src/fuzz/fuzzer.pl
@@ -1,3 +1,72 @@
 #!/usr/bin/perl
 #
 # Read a program and randomly replace, add, remove elements
+
+use strict;
+use warnings;
+use Getopt::Std;
+
+our($opt_t);
+getopts('t');
+
+# References to arrays of tokens
+my @tokens;
+
+my $line = 0;
+while (<>) {
+	@{$tokens[$line]} = tokenize();
+	if ($opt_t) {
+		print join('', @{$tokens[$line]});
+		next;
+	}
+	$line++;
+}
+
+# Return $_ split into tokens
+sub tokenize {
+	return split(/(
+	\w+|		# Identifiers
+	\s+|		# White space
+	\d+|		# Integers
+	0[xb]\d+|
+	\d+\.\d*|	# Simple floating-point numbers
+	\d*\.\d+|
+	\"[^"]*\"|	# Simple strings
+	\'[^"]*\'|
+	\+\+|		# C and C++ multi-character operators
+	\-\-|
+	\-\>|
+	\[\]|
+	\.\*|
+	\-\>\*|
+	\<\<|
+	\>\>|
+	\<\=|
+	\>\=|
+	\=\=|
+	\!\=|
+	\&\&|
+	\|\||
+	\+\=|
+	\-\=|
+	\*\=|
+	\/\=|
+	\%\=|
+	\<\<\=|
+	\>\>\=|
+	\&\=|
+	\^\=|
+	\|\=|
+	\<\=\>|		# Additional Perl operators
+	\=\>|
+	\=\~|
+	\!\~|
+	\*\*|
+	\~\~|
+	\.\.\.|
+	\.\.|
+	\*\*\=|
+	\>\>\>|		# Additional Java operators
+	\>\>\>\=|
+	.)/x);
+}
