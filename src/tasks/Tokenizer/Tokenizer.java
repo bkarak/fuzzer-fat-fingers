ed5b26f06e1c7bcb07c81ed3ae79ac1bd0c4f867
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2012-08-09 16:20:08 +0300

    fixed tokenizer project for Java

--- src/tasks/Tokenizer/Tokenizer.java
+++ src/tasks/Tokenizer/Tokenizer.java
@@ -1,9 +1,12 @@
+import java.util.StringTokenizer;
+
 public class Tokenizer {
 	public static void main(String[] args) {
 		String toTokenize = "Hello,How,Are,You,Today";
 		StringTokenizer tokenizer = new StringTokenizer(toTokenize, ",");
+		
 		while(tokenizer.hasMoreTokens()) {
 			System.out.print(tokenizer.nextToken() + ".");
-}		
+		}		
 	}
 }
--- src/tasks/ZigZag/ZigZag.java
+++ src/tasks/ZigZag/ZigZag.java
@@ -1,31 +1,37 @@
-public static int[][] Zig_Zag(final int size)
-{
- int[][] data = new int[size][size];
- int i = 1;
- int j = 1;
- for (int element = 0; element < size * size; element++)
- {
-  data[i - 1][j - 1] = element;
-  if ((i + j) % 2 == 0)
+
+public class ZigZag {
+  public static int[][] Zig_Zag(final int size)
   {
-   // Even stripes
-   if (j < size)
-    j++;
-   else
-    i+= 2;
-   if (i > 1)
-    i--;
+    int[][] data = new int[size][size];
+    int i = 1;
+    int j = 1;
+
+    for (int element = 0; element < size * size; element++) {
+      data[i - 1][j - 1] = element;
+        
+      if ((i + j) % 2 == 0) {
+        // Even stripes
+        if (j < size)
+          j++;
+        else
+          i += 2;
+     
+      if (i > 1)
+        i--;
+    } else {
+      // Odd stripes
+      if (i < size)
+        i++;
+      else
+        j+= 2;
+      if (j > 1)
+        j--;
+    }
+      return data;
+    }
   }
-  else
-  {
-   // Odd stripes
-   if (i < size)
-    i++;
-   else
-    j+= 2;
-   if (j > 1)
-    j--;
+
+  public static void main(String[] args) {
+    
   }
- }
- return data;
 }
