056da5fb59520bc604db0021ab3cb3ca9ded4002
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2012-08-09 21:03:59 +0300

    adding beers fix pl

--- src/tasks/Beers/Beers.pl
+++ src/tasks/Beers/Beers.pl
@@ -1,14 +1,20 @@
-my $b = 99;
+#!/usr/bin/perl -w
  
-sub b($b) {
-    "$b bottle{'s'.substr($b == 1)} of beer";
-}
+my $verse = <<"VERSE";
+100 bottles of beer on the wall,
+100 bottles of beer!
+Take one down, pass it around!
+99 bottles of beer on the wall!
  
-repeat while --$b {
-    .say for "&b($b) on the wall",
-             b($b),
-             'Take one down, pass it around',
-             "&b($b-1) on the wall",
-             '';
+VERSE
+ 
+{
+    $verse =~ s/(\d+)/$1-1/ge;
+    $verse =~ s/\b1 bottles/1 bottle/g;
+    my $done = $verse =~ s/\b0 bottle/No bottles/g; # if we make this replacement, we're also done.
+ 
+    print $verse;
+    redo unless $done;
 }
+
  
