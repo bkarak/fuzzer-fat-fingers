dbb9a78328a4f5258a0feb6b73f17d64b355a492
Kostas Stroggylos <stroggylos@gmail.com>
2012-08-13 00:51:17 +0300

    Added C# implementation of ZigZag

--- src/tasks/ZigZag/ZigZag.cs
+++ src/tasks/ZigZag/ZigZag.cs
@@ -1,25 +1,52 @@
-public static int[,] ZigZag(int n)
+using System;
+
+namespace ZigZagTest
 {
-    int[,] result = new int[n, n];
-    int i = 0, j = 0;
-    int d = -1; // -1 for top-right move, +1 for bottom-left move
-    int start = 0, end = n * n - 1;
-    do
-    {
-        result[i, j] = start++;
-        result[n - i - 1, n - j - 1] = end--;
- 
-        i += d; j -= d;
-        if (i < 0)
-        {
-            i++; d = -d; // top reached, reverse
-        }
-        else if (j < 0)
-        {
-            j++; d = -d; // left reached, reverse
-        }
-    } while (start < end);
-    if (start == end)
-        result[i, j] = start;
-    return result;
+
+	class ZigZagTest
+	{
+	
+		public static int[,] ZigZag(int n)
+		{
+			int[,] result = new int[n, n];
+			int i = 0, j = 0;
+			int d = -1; // -1 for top-right move, +1 for bottom-left move
+			int start = 0, end = n * n - 1;
+			do
+			{
+				result[i, j] = start++;
+				result[n - i - 1, n - j - 1] = end--;
+		 
+				i += d; j -= d;
+				if (i < 0)
+				{
+					i++; d = -d; // top reached, reverse
+				}
+				else if (j < 0)
+				{
+					j++; d = -d; // left reached, reverse
+				}
+			} while (start < end);
+			if (start == end)
+				result[i, j] = start;
+			return result;
+		}
+		
+		public static void Main(string[] args)
+		{
+			int n = 5;
+			int[,] table = ZigZag(5);
+			
+			for(int row = 0; row < n; row++)
+			{
+				for(int col = 0; col < n; col++)
+				{
+					Console.Write("{0,5}", table[row, col]);
+				}
+				Console.WriteLine();
+			}
+		
+		}
+	}
+	
 }
