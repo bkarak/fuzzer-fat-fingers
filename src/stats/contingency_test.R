19ab7d3ae141a0a7005f838372a1f42c6542b07f
Panos Louridas <louridas@gmail.com>
2012-10-05 11:48:47 +0300

    incorporate discussion

--- src/stats/contingency_test.R
+++ src/stats/contingency_test.R
@@ -12,12 +12,13 @@ for (filename in filenames) {
       m <- data[c(i, j),][-1]
       colnames(m) <- c("passed", "caught")
       rownames(m) <- data[,1][c(i, j)]
+      cat(sprintf("testing %s for %s and %s\n", filename, rownames(m)[1], rownames(m)[2]));
       print(m)
       r <- fisher.test(m)
       cat("result", r$p.value, "\n")
       if (r$p.value >= 0.05) {
         cat(sprintf("%s null hypothesis not rejected for %s and %s, p-value=%f\n", filename, rownames(m)[1], rownames(m)[2], r$p.value));
-      }
+      } 
       cat("=====\n")
     }
   }
