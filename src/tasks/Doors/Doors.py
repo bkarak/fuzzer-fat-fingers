80df88c66463c8270802f43cd0dd17aefe8f0924
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2012-10-28 23:42:12 +0200

    added Doors task

---
+++ src/tasks/Doors/Doors.py
@@ -0,0 +1,7 @@
+ 
+doors = [False] * 100 
+for i in xrange(100):
+    for j in xrange(i, 100, i+1):
+        doors[j] = not doors[j]
+    print "Door %d:" % (i+1), 'open' if doors[i] else 'close'
+ 
