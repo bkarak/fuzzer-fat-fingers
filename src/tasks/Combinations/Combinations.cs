3400570c404fba0033cc963d4f34a25bc0f13027
Strihias <strihias@yahoo.gr>
2012-11-13 00:03:44 +0200

    added Task: Combinations

---
+++ src/tasks/Combinations/Combinations.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+ 
+public class Program
+{
+    public static IEnumerable<int[]> Combinations(int m, int n)
+    {
+            int[] result = new int[m];
+            Stack<int> stack = new Stack<int>();
+            stack.Push(0);
+ 
+            while (stack.Count > 0) {
+                int index = stack.Count - 1;
+                int value = stack.Pop();
+ 
+                while (value < n) {
+                    result[index++] = value++;
+                    stack.Push(value);
+                    if (index == m) {
+                        yield return result;
+                        break;
+                    }
+                }
+            }
+    }
+ 
+    static void Main()
+    {
+        foreach (int[] c in Combinations(3, 5))
+        {
+            for (int i = 0; i < c.Length; i++)
+            {
+                Console.Write(c[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
