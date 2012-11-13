38fc4f265bbaf1172dad89bb4f0aa127fe8fe819
Strihias <strihias@yahoo.gr>
2012-11-13 13:35:49 +0200

    added nea Task:CountOccurences

---
+++ src/tasks/CountOccurences/CountOccurences.cs
@@ -0,0 +1,25 @@
+using System;
+ 
+class SubStringTestClass
+{
+   public static int CountSubStrings(this string testString, string testSubstring)
+        {
+            int count = 0;
+ 
+            if (testString.Contains(testSubstring))
+            {
+                for (int i = 0; i < testString.Length; i++)
+                {
+                    if (testString.Substring(i).Length >= testSubstring.Length)
+                    {
+                        bool equals = testString.Substring(i, testSubstring.Length).Equals(testSubstring);
+                        if (equals)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+}
---
+++ src/tasks/CountOccurences/CountOccurences.rb
@@ -0,0 +1,4 @@
+irb(main):001:0> "the three truths".scan("th").length
+=> 3
+irb(main):002:0> "ababababab".scan("abab").length
+=> 2
