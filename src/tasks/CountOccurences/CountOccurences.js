fd7dca83f120b281e371acafb1af16e472200b61
Strihias <strihias@yahoo.gr>
2012-11-29 14:23:49 +0200

    added Fortran Code in Task:Substring

--- src/tasks/CountOccurences/CountOccurences.js
+++
@@ -1,3 +0,0 @@
-function countSubstring(str, subStr){
-	return str.match(new RegExp(subStr, "g")).length
-}
--- src/tasks/CountOccurences/CountOccurences.rb
+++
@@ -1,4 +0,0 @@
-irb(main):001:0> "the three truths".scan("th").length
-=> 3
-irb(main):002:0> "ababababab".scan("abab").length
-=> 2
