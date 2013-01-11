9f9a69111dbc628ef28370df3b11d81a432db213
Strihias <strihias@yahoo.gr>
2013-01-11 21:03:26 +0200

    added new Task:ListSearch

---
+++ src/tasks/ListSearch/ListSearch.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.Arrays;
+ 
+ public class ListSearch{
+ 	public static void main( String args[]){
+ 
+List<String> haystack = Arrays.asList("Zig","Zag","Wally","Ronald","Bush","Krusty","Charlie","Bush","Bozo");
+ 
+for (String needle : new String[]{"Washington","Bush"}) {
+    int index = haystack.indexOf(needle);
+    if (index < 0)
+        System.out.println(needle + " is not in haystack");
+    else
+        System.out.println(index + " " + needle);
+}
+}
+}
