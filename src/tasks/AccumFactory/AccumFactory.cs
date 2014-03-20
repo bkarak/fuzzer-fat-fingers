20a7c9a9aa8c91677356e741a06151a1a946715d
Strihias <strix@aiolos.dmst.aueb.gr>
2014-03-20 10:43:35 -0400

    completed AccumFactory.cs

--- src/tasks/AccumFactory/AccumFactory.cs
+++ src/tasks/AccumFactory/AccumFactory.cs
@@ -1,9 +1,19 @@
 using System;
+
+ class AccumFactory {
+    private double sum;
+
+     AccumFactory(double sum0) {
+        sum = sum0;
+    }
+
+     double call(double n) {
+        return sum += n;
+    }
  
-class Program
-{
-    static void Main(string[] args)
-    {
-        
+     static void Main(String[] args) {
+        AccumFactory x = new AccumFactory(1);
+        x.call(5);
+        Console.WriteLine(x.call(2.3));
     }
 }
