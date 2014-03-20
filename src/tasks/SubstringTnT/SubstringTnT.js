ed078637174a6968e9be266f7d9efac6ce5c1c72
Strihias <strix@aiolos.dmst.aueb.gr>
2014-03-20 12:05:20 -0400

    minor change in SubstringTnT.js

--- src/tasks/SubstringTnT/SubstringTnT.js
+++ src/tasks/SubstringTnT/SubstringTnT.js
@@ -1,3 +1,3 @@
-alert("knight".slice(1));       // strip first character
-alert("socks".slice(0, -1));    // strip last character
-alert("brooms".slice(1, -1));   // strip both first and last characters
+document.write("knight".slice(1));       // strip first character
+document.write("socks".slice(0, -1));    // strip last character
+document.write("brooms".slice(1, -1));   // strip both first and last characters
